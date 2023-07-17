473fbbf9e072eb83a751344031c5e7294ffbab86
Lolorpof <lolorpof@gmail.com>
2023-07-17 16:31:38 +0700

    add components

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,63 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Task } from "../../components/Task";
+import { TaskInput } from "../../components/TaskInput";
+
 export default function Home() {
   return (
     // Main container
     <div className="container mx-auto">
       {/* header section */}
-      <div>
+      <Header></Header>
+      {/* <div>
         <h4 className="display-4 fst-italic text-center">Todo List Mock Up</h4>
         <p className="text-secondary text-center">
           This lab teach you how to split react component(s)
         </p>
-      </div>
+      </div> */}
 
       {/* tasks container */}
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* task input */}
-        <div className="d-flex gap-1">
+        <TaskInput></TaskInput>
+        {/* <div className="d-flex gap-1">
           <input
             className="form-control"
             placeholder="Insert a task here.."
           ></input>
           <button className="btn btn-primary">Add</button>
-        </div>
+        </div> */}
+
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Read a book</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Read a book"></Task>
+
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+        <Task title="Take a shower"></Task>
+        {/* <div className="d-flex p-3 gap-2 align-items-center border-bottom">
           <span>Take a shower</span>
           <button className="btn btn-success">Done</button>
           <button className="btn btn-danger">Delete</button>
-        </div>
+        </div> */}
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+        <Task title="Sleep"></Task>
+        {/* <div className="d-flex p-3 gap-2 align-items-center border-bottom">
           <span>Sleep</span>
           <button className="btn btn-success">Done</button>
           <button className="btn btn-danger">Delete</button>
-        </div>
+        </div> */}
       </div>
 
       {/* //footer section */}
-      <div className="mt-3">
+      <Footer
+        year="2023"
+        fullname="Sirawit Sirabanchongkran"
+        studentId="650610810"
+      ></Footer>
+      {/* <div className="mt-3">
         <p className="text-secondary text-center">
           copyright © 2023 Chayanin Suatap 12345678
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
